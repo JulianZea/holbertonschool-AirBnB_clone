654799c75f23c92d09f0108100917593d7f796d5
DaisyGeraldine <chipanal@hotmail.com>
2022-10-16 23:32:14 -0500

    add Unitest test

---
+++ tests/test_Base_Model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unittest for class BaseModel()
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class Test_Base(unittest.TestCase):
+    def test_Base_Model(self):
+        """Commment"""
